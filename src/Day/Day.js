887b637afa8ac965672a163ac46a37e81658c034
Jeff Goodall <goodallhj@gmail.com>
2018-04-04 16:26:18 -0600

    Adding pop up on click for the event details in calendar

--- src/Day/Day.js
+++ src/Day/Day.js
@@ -7,10 +7,10 @@ export class Day extends Component {
     super(props);
     this.state = {
       events: [
-        { day: 1, startTime: 1300, endTime: 1400 },
-        { day: 1, startTime: 1300, endTime: 1400 },
-        { day: 13, startTime: 1300, endTime: 1400 },
-        { day: 23, startTime: 1300, endTime: 1400 }
+        { day: 1, startTime: '1:00 pm', endTime: '2:00 pm', name: 'Lunch', isClicked:'false'},
+        { day: 1, startTime: '11:00 am', endTime: '12:00 pm', name: 'Appointment', isClicked:'false'},
+        { day: 13, startTime: '10:00 am', endTime: '10:30 am', name: 'Dentist', isClicked:'false'},
+        { day: 23, startTime: '4:00 pm', endTime: '5:00 pm', name: 'Meeting', isClicked:'false'}
       ]
     };
   }
@@ -27,14 +27,16 @@ export class Day extends Component {
     }
   }
 
-  handleClick(event) {
-    console.log('clicked');
+  handleDblClick() {
+    console.log('double click');
+    
   }
 
   render() {
     const { month, date } = this.props;
     return (
-      <div className="day" onClick={this.handleClick}>
+      <div className="day" ondblclick={this.handleDblClick}
+      >
         <span className="date">{this.props.date}</span>
         {this.addEvents()}
       </div>
--- src/Event/Event.js
+++ src/Event/Event.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './Event.css';
 
-const Event = () => {
-  return (
-    <div>
-      EVENT
-    </div>
-  );
-};
+export class Event extends Component {
+
+  handleClick = () => {
+    
+    
+  }
+  render() {
+    const {event} = this.props;
+    return (
+      <div className='Event' onClick={this.handleClick}>
+        <h4>{event.name}</h4>
+      </div>
+    )
+  }
+}
 
 export default Event;
+
