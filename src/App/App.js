5b5fe99e0953af81be3ab33d09e2e9205863ae0f
Jeff Goodall <goodallhj@gmail.com>
2018-04-04 13:27:52 -0600

    Header Complete no style

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.date();
+  }
+
+  date() {
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+    const longMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return `${longMonth[month -1]} ${year}`;
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>APPPPPPPPPP</h1>
+        <header>
+          <h1>{this.date()}</h1>
+          <button class="arrowLeft"></button>
+          <button>Today</button>
+          <button class="arrowright"></button>
+        </header>
+        
       </div>
     );
   }
