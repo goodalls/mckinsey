08635aa436c1a542236ae8083a367447080c7bab
Jeff Goodall <goodallhj@gmail.com>
2018-04-04 14:22:39 -0600

    adding state to date for month

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
+import Month from '../Month/Month';
 
 class App extends Component {
-  componentDidMount() {
-    this.date();
+  constructor(props) {
+    super(props);
+    this.state = {
+      today: new Date(),
+      month: new Date().getMonth(),
+      year: new Date().getFullYear()
+    };
   }
 
   date() {
     const today = new Date();
     const month = today.getMonth();
-    const year = today.getFullYear();
-    const longMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    return `${longMonth[month -1]} ${year}`;
+    const longMonth = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    return longMonth[month];
+  }
+
+  daysInMonth() {
+    const { year, month } = this.state;
+    return new Date(year, month, 0).getDate();
   }
 
   render() {
+    const { year } = this.state;
     return (
       <div className="App">
         <header>
-          <h1>{this.date()}</h1>
+          <h1><span className="month">{this.date()}</span> {year}</h1>
           <div className="navButtons">
             <button className="arrowLeft">&#8249;</button>
             <button className="today">Today</button>
             <button className="arrowright">&#8250;</button>
           </div>
         </header>
-        <div>Sun</div>
-        <div>Mon</div>
-        <div>Tue</div>
-        <div>Wed</div>
-        <div>Thu</div>
-        <div>Fri</div>
-        <div>Sat</div>
+        <div className="weekDays">Sun</div>
+        <div className="weekDays">Mon</div>
+        <div className="weekDays">Tue</div>
+        <div className="weekDays">Wed</div>
+        <div className="weekDays">Thu</div>
+        <div className="weekDays">Fri</div>
+        <div className="weekDays">Sat</div>
+        <Month days={this.daysInMonth()} />
       </div>
     );
   }
---
+++ src/Month/Month.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Day from '../Day/Day';
+import './Month.css';
+
+const Month = () => {
+
+  return (
+    <div className='Month'>
+      Month
+    </div>
+  );
+};
+
+export default Month;
